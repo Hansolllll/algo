65d100402d6b37d5ab665f940b31bfbd555b766b
hansol <breezy_green@naver.com>
2023-10-20 20:04:26 +0900

    update

--- swea/4869_종이붙이기/sol.py
+++ swea/4869_종이붙이기/sol.py
@@ -3,16 +3,12 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 for tc in range(1, 1+T):
-    width = int(input())  # 주어진 직사각형 가로 크기
-    height = 20 # 고정된 세로 크기 
-    dimension = 20 * int(input())
+    N = int(input()) // 10
 
-    # 직사각형 종이(width * height, dimension) 
-    # 10*20,200 / 20*20,400
-    di1 = 200
-    di2 = 400
+    memo = [0, 1, 3]
 
-    dp = [0]*100
-    dp[0] = 0
+    while N >= len(memo):
+        result = memo[len(memo)-2] * 2 + memo[len(memo)-1]
+        memo.append(result)
 
-    for num in dp:
+    print(memo[N])
