ca510df0cdc7145e765196699f2d97352530c256
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-10-26 21:05:04 +0900

    문제 풀이 추가

---
+++ programmers/1.입문/A로B만들기/sol.py
@@ -0,0 +1,16 @@
+def solution(before, after):
+    # before에 있는 문자열이 after에 있다면 
+    for char in before:
+        if char in after:
+            # replace 이용해서 1개만 '0'으로 바꾸기
+            before = before.replace(char, '0', 1)
+            after = after.replace(char, '0', 1)
+
+    # 반복문 종료 후 before와 after가 서로 같다면 문자열 내부 문자 갯수가 동일하단 의미
+    if before == after:
+        return 1
+    else:
+        return 0
+
+print(solution('olleh', 'hello'))
+print(solution('allpe', 'apple'))
---
+++ programmers/2.알고리즘고득점kit/H-index/sol.py
@@ -0,0 +1,37 @@
+def solution(citations):
+    #?
+    # citations.sort(reverse=True)
+    # for i in range(len(citations)):
+    #     if i + 1 >= citations[i]:
+    #         return citations[i] 
+
+
+    # 이중반복문으로 i인덱스 이상인 수를 카운트 해서 count_list에 넣기(H-index에 대한 이해부족)
+    # answer = []
+    # count_list = []
+    
+    # for i in range(len(citations)):
+    #     count = 0
+    #     for j in range(len(citations)):
+    #         if citations[j] >= citations[i]:
+    #             count += 1
+    #     count_list.append(count)
+
+    # for k in range(len(citations)):
+    #     if count_list[k] >= citations[k]:
+    #         answer.append((citations[k], count_list[k]))
+
+    # return max(answer)[0]
+    
+    # gpt 나쁜놈
+    # citations.sort()  # 논문 인용 횟수 배열을 오름차순으로 정렬
+    # n = len(citations)
+    
+    # for i in range(n):
+    #     if citations[i] >= n - i:
+    #         return n - i
+    
+    # return 0
+
+
+print(solution([3, 0, 6, 1, 5]))
