aaa26010ffedc044eaf0ac33b64d87d2c00bb69c
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-10-30 02:48:15 +0900

    수정

--- programmers/1.입문/한번만등장한문자/sol.py
+++ programmers/1.입문/한번만등장한문자/sol.py
@@ -1,24 +1,18 @@
 def solution(s):
-    # stack = []
-    # for char in s:
-    #     if s.count(char) == 1:
-    #         stack.append(char)
+    # s 안에 1개만 있는 문자만 stack에 추가
+    stack = []
+    for char in s:
+        if s.count(char) == 1:
+            stack.append(char)
+
+        stack.sort() # 문자 순서대로 정렬
+        return ''.join(stack) # 문자열로 출력 
+
+    # 짧은버전
     stack = [char for char in s if s.count(char) == 1]
     stack.sort()
-
-        # stack.sort()
     return ''.join(stack)
 
-
-
-
-    # for char2 in s:
-    #     num = s.count(char2)
-    #     if num >= 2:
-    #         s = s.replace(char2, '', num)
-            # stack.remove(char2)
-
-    return stack
 
 print(solution('abcabcadc'))
 print(solution('abdc'))
