d8e2ebc0b27fd176c7a50ba48b1495bb132cb358
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-10-29 23:29:44 +0900

    문제풀이 추가

---
+++ programmers/1.입문/가까운수/sol.py
@@ -0,0 +1,11 @@
+def solution(array, n):
+    answer = 100
+    for num in array:
+        if abs(num - n) < abs(answer - n):
+            answer = num
+        elif abs(num - n) == abs(answer - n):
+            answer == min(num, answer)
+    return answer
+
+print(solution([3, 10, 28], 20))
+print(solution([10, 11, 12], 13))
