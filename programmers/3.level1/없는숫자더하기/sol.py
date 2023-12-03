cef30088951bfe9459cb75d64c4cdcdd39dd2ba1
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-12-03 23:39:15 +0900

    없는 숫자 더하기 문제풀이

---
+++ programmers/3.level1/없는숫자더하기/sol.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    num_list = [i for i in range(10)]
+    for num in numbers:
+        if num in num_list:
+            num_list.remove(num)
+
+    return sum(num_list)
+
+print(solution([1,2,3,4,6,7,8,0]))
+print(solution([5,8,4,0,6,7,9]))
