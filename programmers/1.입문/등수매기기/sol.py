2b4cd108c101a51f147476414de9e17e4c1e7024
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-12-04 20:35:15 +0900

    스터디 문제풀이 추가

--- programmers/1.입문/등수매기기/sol.py
+++ programmers/1.입문/등수매기기/sol.py
@@ -12,5 +12,24 @@ def solution(score):
 
     return score
 
+# 하이브리드 소트 ?
+def solution(scores):
+    answer = []
+    
+    for i, score in enumerate(scores, start=1): #각 학생에게 1번부터 번호표 부착
+        avg_score = sum(score) / len(score) #각 학생의 평균 계산(len(score)대신 2를 넣어도 됨.)
+        
+        rv_student = 0 # rv = Reference value(등수)
+
+        for student in scores:
+            rv_score = sum(student) / len(student) # 전체 평균을 구해서 rv_score 작성
+
+            if rv_score > avg_score: # 학생 score가 평균보다 작으면
+                rv_student += 1 # 등수를 하나 올려준다.(1등보다2등이 낮은 거임, 숫자가 올라갈수록 낮은 등수)
+
+        answer.append(rv_student + 1)
+
+    return answer
+
 print(solution([[80, 70], [90, 50], [40, 70], [50, 80]]))
 print(solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
