46d74f614069515145dc577d8c27076b89344d8a
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-12-11 00:57:35 +0900

    연속된 수의 합 문제풀이

---
+++ programmers/1.입문/연속된수의합/sol.py
@@ -0,0 +1,18 @@
+def solution(num, total):
+    # total 범위 잡기
+    for i in range(-100, total):
+        answer = []
+        # 1개 숫자인 경우엔 바로 total return하기
+        if num == 1:
+            return [total]
+        # 여러개의 숫자 필요한 경우 answer 리스트에 넣고 그 합을 비교하기
+        else:
+            for j in range(num):
+                answer.append(i+j)
+            if sum(answer) == total:
+                return answer
+            
+print(solution(3, 12))
+print(solution(5, 15))
+print(solution(4, 14))
+print(solution(5, 5))
