19fc642fc067ced102b097a7b76540ec5afb7421
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-12-21 23:57:16 +0900

    문자열 다루기 기본 문제풀이 추가

--- programmers/3.level1/문자열다루기기본/sol.py
+++ programmers/3.level1/문자열다루기기본/sol.py
@@ -1,10 +1,10 @@
 def solution(s):
-    if len(s) != 4 or len(s) != 6:
-        return False
-    for char in s:
-        if char.isdigit()==False:
-            return False
-    return True
+    if len(s) = 4 or len(s) = 6:
+        for char in s:
+            if char.isdigit()==False:
+                return False
+        return True
+    return False
 
 print(solution('a234'))
 print(solution('1234'))
