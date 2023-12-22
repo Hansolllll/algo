aa2a4287cfed07334f0ae8f69afb12371c1a816b
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-12-23 02:01:41 +0900

    문제풀이 추가

--- programmers/3.level1/부족한 금액 계산하기/sol.py
+++ programmers/3.level1/부족한 금액 계산하기/sol.py
@@ -3,8 +3,8 @@ def solution(price, money, count):
     for i in range(count):
         charge = price * (i+1)
         total += charge
+        
     return (total-money) if total>money else 0
 
 
 print(solution(3, 20, 4))
-
