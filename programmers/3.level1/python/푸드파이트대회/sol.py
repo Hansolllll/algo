372103ad04d9897fa34f93daa96065bc0bc64e14
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-01-23 23:32:54 +0900

    푸드파이트대회 문제풀이 방법 추가

--- programmers/3.level1/python/푸드파이트대회/sol.py
+++ programmers/3.level1/python/푸드파이트대회/sol.py
@@ -1,12 +1,20 @@
 def solution(food):
+    # 풀이 1 
     answer = ''
+    # food.index(num)으로 할 경우 동일한 숫자가 2개 이상 있을 시 
+    # 인덱스가 제대로 반환되지 않음 
     idx = 0
+    # food에서 1번 음식부터 반복
     for num in food[1:]:
         idx += 1
+        # 음식 번호를 (음식갯수/2)의 몫만큼만 반복 
         answer += str(idx) * (num // 2)
-
+    # 왼쪽부터 먹는 배치 + 물 + 오른쪽부터 먹는 배치  
     return answer+'0'+answer[::-1]
 
+    # 풀이 2 
+    # 리스트 컴프리헨션 
+    return ''.join([str(i)*(food[i]//2) for i in range(1, len(food))]  + ['0'] +  [str(i)*(food[i]//2) for i in range(1, len(food))][::-1])
 
 print(solution([1,3,4,6]))
 print(solution([1,7,1,2]))
