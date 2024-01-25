118703f3df28b7b57916f1fee00cbf4af77ce44f
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-01-26 01:26:57 +0900

    1차 비밀번호 문제풀이

--- programmers/3.level1/python/1차비밀번호/sol.py
+++ programmers/3.level1/python/1차비밀번호/sol.py
@@ -1,6 +1,25 @@
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(len(arr1))
+    # 이진수로 변환하기 
+    for i in range(len(arr1)):
+        arr1[i] = bin(arr1[i])[2:]
+        arr2[i] = bin(arr2[i])[2:]
+
+        while len(arr1[i]) < n:
+            arr1[i] = '0' + arr1[i]
+        while len(arr2[i]) < n:
+            arr2[i] = '0' + arr2[i]
+    # arr 안에서 이진수 자릿수가 모두 0일 때를 제외하고 array에 #추가 
+    for i in range(len(arr1)):
+        array = ''
+        for j in range(n):
+            if arr1[i][j] == '0' and arr2[i][j] == '0':
+                array += ' '
+            else: 
+                array += '#'
+        # answer에 array 추가 
+        answer.append(array)
+
     return answer
 
 print(solution(5, [9,20,28,18,11], [30,1,21,17,28]))
