617d539c900a187980e80aa310aa7368348bbef4
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-01-30 01:38:31 +0900

    명예의 전당1 문제풀이

--- programmers/3.level1/python/명예의전당1/sol.py
+++ programmers/3.level1/python/명예의전당1/sol.py
@@ -1,3 +1,21 @@
 def solution(k, score):
-    answer = []
-    return answer
+    answer = score[:k]
+    low_score = []
+    # k이전
+    # 점수들 중 가장 낮은 점수를 low_score에 추가
+    for _ in range(len(answer)):
+        low_score.append(min(answer[:_+1]))
+    # k 이후 
+    # answer에서 가장 낮은 수와 점수비교해서 더 크면 answer에 추가
+    # answer에서 가장 낮은 점수 찾아서 low_scoer에 추가
+    for i in range(k, len(score)):
+        if score[i] > min(answer):
+            answer.remove(min(answer))
+            answer.append(score[i])
+            low_score.append(min(answer))
+        else:
+            low_score.append(min(answer))
+    return low_score
+
+print(solution(3,[10,100,20,150,1,100,200]))
+print(solution(4,[0,300,40,300,20,70,150,50,500,1000]))
