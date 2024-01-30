fe49d7fb6c9f514e1610d77e0abb9388b1670b64
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-01-31 00:59:17 +0900

    카드뭉치 문제풀이

---
+++ programmers/3.level1/python/카드뭉치/sol.py
@@ -0,0 +1,7 @@
+def solution(cards1, cards2, goal):
+    answer = ''
+    
+    return answer
+
+print(solution(["i", "drink", "water"],["want", "to"],["i", "want", "to", "drink", "water"]))
+print(solution(["i", "water", "drink"],["want", "to"],["i", "want", "to", "drink", "water"]))
