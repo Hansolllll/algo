97fe0d8635619084a80ce9ca95dd7fb95d3dfba3
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-02-02 00:21:35 +0900

    문제풀이

--- baekjoon/17608/sol.py
+++ baekjoon/17608/sol.py
@@ -1,16 +1,17 @@
 import sys
-sys.stdin = open('input.txt')
-
+# sys.stdin = open('input.txt')
+def input():
+    return sys.stdin.readline().rstrip()
+    
 TC = int(input())
 blocks = []
 
 for i in range(TC):
-    num = int(input())
-    blocks.append(num)
+    blocks.append(int(input()))
 
 count = 1
+std = blocks[-1]
 for j in blocks[::-1]:
-    std = blocks[-1]
     if j > std:
         count += 1
         std = j
