9425e9ec304c9c623f1d589de3fd1bb0e9700c0b
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-02-07 01:24:31 +0900

    폰켓몬 문제풀이 추가

--- programmers/3.level1/python/폰켓몬/sol.py
+++ programmers/3.level1/python/폰켓몬/sol.py
@@ -1,6 +1,24 @@
+from itertools import permutations
+# 문제파악 다시 하기 
+# permutation을 쓸 이유가 없음 
 def solution(nums):
-    answer = 0
-    return answer
+    # 골라야하는 폰켓몬의 수가 폰켓몬 종류보다 많다면 폰켓몬 종류 수 만큼 리턴
+    if len(nums)//2 >= len(set(nums)):
+        return len(set(nums))
+    # 고를 폰켓몬 수 보다 주어진 종류가 더 많다면 고를 마리 수만큼 리턴
+    else:
+        return len(nums)//2
+
+# 시간초과 2
+# def solution(nums):
+#     return max(list(map(len,(map(set,permutations(nums, len(nums)//2))))))
+
+# 시간초과 1
+# def solution(nums):
+#     pick = list(permutations(nums, len(nums)//2))
+#     for i in range(len(pick)):
+#         pick[i] = len(set(pick[i]))
+#     return max(pick)
 
 print(solution([3,1,2,3]))
 print(solution([3,3,3,2,2,4]))
