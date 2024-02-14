53767c98883576568cb0cf4daa12ff1453672d5b
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-02-15 00:27:16 +0900

    소수만들기 문제풀이

--- programmers/3.level1/python/소수만들기/sol.py
+++ programmers/3.level1/python/소수만들기/sol.py
@@ -1,7 +1,24 @@
-def solution(nums):
-    answer = -1
+from itertools import combinations
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+def solution(nums):
+    answer = 0
+    # nums에서 3개 뽑아 그 합을 num에 지정
+    for x,y,z in list(combinations(nums, 3)):
+        num = x+y+z
+        count = 0
+        # num의 제곱근까지만 반복 
+        for _ in range(1, int(num**(1/2))+1):
+            if num % _ == 0:
+                count += 1
+                if (_**2) != num:
+                    count += 1
+        
+        # num의 약수가 2개라면 answer 갯수 +1
+        if  count == 2:
+            answer += 1
 
     return answer
+                
+
+print(solution([1,2,3,4]))
+print(solution([1,2,7,6,4]))
