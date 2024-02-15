1040dbf32e52e8c7a34224494f4c745ccdb1826d
parkhansol <145949904+hansolki@users.noreply.github.com>
2024-02-16 00:19:50 +0900

    소수찾기 문제풀이

--- programmers/3.level1/python/소수찾기/sol.py
+++ programmers/3.level1/python/소수찾기/sol.py
@@ -1,3 +1,30 @@
 def solution(n):
     answer = 0
+    # 2부터 소수판별
+    for i in range(2, n+1):
+        # i의 제곱근까지만 반복 
+        # 소수는 1과 본인만 가지므로 이외 약수 있으면 break
+        for k in range(2, int(i**0.5)+1):
+            if i % k == 0:
+                break
+        # break만나지 않고 끝나면 answer +1 
+        else:
+            answer += 1
     return answer
+        
+
+    # 시간초과 
+    # answer = 0
+    # for i in range(2, n+1):
+    #     count = 1
+    #     for k in range(2, int(i**0.5)+1):
+    #         if i % k == 0:
+    #             count += 1
+            
+    #     if count == 1:
+    #         answer += 1 
+
+    # return answer
+
+print(solution(10))
+print(solution(5))
